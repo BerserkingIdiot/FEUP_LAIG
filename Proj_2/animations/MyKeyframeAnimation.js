036ca7e80723b7b72b0a9c5d7897bff1035bcac1
Cajo Albuquerque <up201706735@fe.up.pt>
2019-10-21 16:15:34 +0100

    MyKeyframeAnimation further work

--- Proj_2/animations/MyKeyframeAnimation.js
+++ Proj_2/animations/MyKeyframeAnimation.js
@@ -4,7 +4,7 @@
  * @param scene - Reference to XMLScene object
  * @param id - ID of the animation
  * @param numKF - Number of keyframes used on the animation
- * @param keyframes - Array with 
+ * @param keyframes - Array with MyKeyframe objects
 */
 class MyKeyframeAnimation extends MyAnimation {
     constructor(scene, id, numKF, keyframes){
@@ -12,10 +12,31 @@ class MyKeyframeAnimation extends MyAnimation {
         this.id = id;
         this.numKF = numKF;
         this.keyframes = keyframes;
+        // Transformation matrix at each instant
         this.transfMatrix = mat4.create();
+        // Keyframes to the left and to the right of the current instant
+        this.leftKF = -1;
+        this.rightKF = 0;
     }
     update(t) {
+        // This first section adjusts the previous and next keyframes
+        // according to the instant passed as argument.
+        if(t > this.keyframes[this.rightKF].instant){
+            while(true){
+                this.leftKF++;
+                this.rightKF++;
+                if(t < this.keyframes[this.rightKF].instant){
+                    break;
+                }
+            }
+        }
 
+        // Checking if we are using the first keyframe
+        if(this.leftKF == -1){
+            //TODO: add interpolation functions for v0
+        } else {
+            //TODO: add interpolation functions
+        }
     }
     apply() {
         this.scene.multMatrix(this.transfMatrix);
