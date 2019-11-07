f01d07cb0f202dcc5a01869528864c5bf86d98ea
CajoAlbuquerque <carlosjo.da@gmail.com>
2019-11-07 17:13:16 +0000

    MyPatch added

---
+++ Proj_2/primitives/MyPatch.js
@@ -0,0 +1,65 @@
+/**
+ * MyPatch
+ * @constructor
+ * @param scene - Reference to MyScene object
+ * @param id - Primitive id of the object
+ * @param npartsU - Number of parts in U-axis (or X-axis)
+ * @param npartsV - Number of parts in V-axis (or Y-axis)
+ * @param points - Array containing all the points (arrays) of the object
+ */
+class MyPatch extends CGFobject {
+	constructor(scene, id, npointsU, npointsV, npartsU, npartsV, points) {
+        super(scene);
+        
+		this.npointsU = npointsU;
+		this.npointsV = npointsV;
+		this.npartsU = npartsU;
+		this.npartsV = npartsV;
+		this.points = points;
+
+		this.initObject();
+	}
+    /**
+	 * @method initObject
+     * Creates the NURBS object with the specified atributes
+     */
+	initObject() {
+        let degreeU = this.npointsU - 1;
+        let degreeV = this.npointsV - 1;
+
+        let controlPoints = this.arrangeControlPoints();
+
+        console.log("Degree U: " + degreeU);
+        console.log("Degree V: " + degreeV);
+
+        let surface = new CGFnurbsSurface(degreeU, degreeV, controlPoints);
+        
+        this.nurbObject = new CGFnurbsObject(this.scene, this.npartsU, this.npartsV, surface)
+    }
+    /**
+	 * @method arrangeControlPoints
+     * Arranges an array of control points according to U and V degrees.
+     * The returned array is a valid CGFnurbsSurface 3rd argument.
+     */
+    arrangeControlPoints() {
+        let pointsU = [];
+        let pointsIndex = 0;
+
+        for (let u = 0; u < this.npointsU; u++) {
+            let pointsV = [];
+            for (let v = 0; v < this.npointsV; v++) {
+                pointsV.push([...this.points[pointsIndex], 1]);
+                pointsIndex++;
+            }
+            pointsU.push(pointsV);
+        }
+
+        console.table("CONTROL POINTS: " + pointsU);
+
+        return pointsU;
+    }
+
+    display() {
+        this.nurbObject.display();
+    }
+}
