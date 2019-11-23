4456fec8664fa1d99a4c0327a0d13fa64df4fa05
Cajo Albuquerque <carlosjo.da@gmail.com>
2019-11-23 01:57:48 +0000

    Camera with noise effects; Added a second texture to SecurityCamera

--- Proj_2/MySecurityCamera.js
+++ Proj_2/MySecurityCamera.js
@@ -5,14 +5,17 @@ class MySecurityCamera extends CGFobject {
     constructor(scene, texture) {
         super(scene);
 
-        this.rectangle = new MyRectangle(this.scene, 0, 0.5, 1, -1, -0.5);
+        this.rectangle = new MyRectangle(scene, 0, 0.5, 1, -1, -0.5);
         //Normalizing the rectangle texCoords
         this.rectangle.updateTexCoords(0.5, 0.5);
         this.texture = texture;
-        this.cameraShader = new CGFshader(this.scene.gl, "shaders/camera.vert", "shaders/camera.frag");
+        this.noiseText = new CGFtexture(scene, "scenes/images/noise.jpg")
+        this.cameraShader = new CGFshader(scene.gl, "shaders/camera.vert", "shaders/camera.frag");
+
+        this.cameraShader.setUniformsValues({ noiseTexture: 1});
     }
     updateShader(t) {
-        this.cameraShader.setUniformsValues({ currentTime: t / 100 % 2});
+        this.cameraShader.setUniformsValues({ currentTime: (t / 1000) % 10});
     }
     display() {
         this.scene.pushMatrix();
@@ -21,7 +24,9 @@ class MySecurityCamera extends CGFobject {
 		this.scene.setActiveShader(this.cameraShader);
         
         this.texture.bind();
+        this.noiseText.bind(1);
         this.rectangle.display();
+        this.noiseText.unbind(1);
         this.texture.unbind();
         
         // restore default shader (will be needed for drawing the axis in next frame)
