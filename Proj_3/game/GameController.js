ae094fb5bda0fcde46e7269c6ddca1a7c2857498
CajoAlbuquerque <carlosjo.da@gmail.com>
2019-12-02 16:33:44 +0000

    Game end functions and stack pushing

--- Proj_3/game/GameController.js
+++ Proj_3/game/GameController.js
@@ -4,25 +4,30 @@ class GameController {
         this.currentState;
         this.player1Dif = player1Dif;
         this.player2Dif = player2Dif;
-        this.server;
+        this.server = Server();
     }
     gameLoop(){
         this.initGame();
 
-        while(true){ //TODO: game end
+        while(this.gameEnded){ //TODO: game end
             //Check player
             //Display
             //IF PLAYER -> handlePick : aiPlay_prolog
             //execute_play (animation, prolog, change state)
-            this.playPiece(input)
+
+            // Saves a copy of the current state in the stack for Undo and Replay purposes
+            this.plays.push(Object.assign({}, this.currentState));
+
+            if(this.server.checkGameEnd) {
+                this.gameEnded = true;
+            }
         }
 
         //RESET
     }
     initGame() {
         this.plays = [];
-        this.currentState = new GameState([Array(8).fill(Array(8).fill(0)), Array(7).fill(Array(7).fill(0))], [1,0])
-    }
-    playPiece() {
+        this.currentState = new GameState([Array(8).fill(Array(8).fill(0)), Array(7).fill(Array(7).fill(0))], [1,0]);
+        this.gameEnded = false;
     }
 }
