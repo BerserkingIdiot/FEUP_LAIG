b5def0e700806e7eeafe71ffb2acbd768b3d7665
Cajo Albuquerque <carlosjo.da@gmail.com>
2020-01-04 13:20:12 +0000

    Added more states to TurnSM

--- Proj_3/game/TurnStateMachine.js
+++ Proj_3/game/TurnStateMachine.js
@@ -3,14 +3,16 @@ class TurnStateMachine {
         this.scene = scene;
         this.piece;
         this.destinationTile;
-        // 0-> awaiting piece pick,
-        // 1-> awaiting tile pick,
-        // 2-> awaiting arc animation start,
-        // 3-> awaiting arc animation end,
-        // 4-> awaiting grow animation start,
-        // 5-> awaiting grow animation end,
-        // 6-> 
-        // -1-> game start
+        // 0-> awaiting piece pick (drop animation finished)
+        // 1-> awaiting tile pick
+        // 2-> starting arc animation and play request
+        // 3-> play request returned; awaiting animation end
+        // 4-> arc animation end; awaiting request return
+        // 5-> arc animation and play request returned; starting grow animation and checkGameEnd request
+        // 6-> checkGameEnd returned; awaiting animation end
+        // 7-> grow animation ended; awaiting request return
+        // 8-> grow animation and checkGameEnd returned; starting updateTurns request
+        // 9-> updateTurns returned; starting drop animation
         this.state = 0;
     }
     getTile() {
@@ -37,13 +39,55 @@ class TurnStateMachine {
         }
     }
 
-    startAnimation() {
-        if(this.state == 2){
-            this.state = 3;
+    arcAnimationOver() {
+        if(this.state == 2) {
+            this.state = 4;
+        }
+        else if (this.state == 3) {
+            this.state = 5;
         }
     }
 
-    clean(){
+    playRequestOver() {
+        if(this.state == 2) {
+            this.state = 3;
+        }
+        else if (this.state == 4) {
+            this.state = 5;
+        }
+    }
+
+    growAnimationOver() {
+        if(this.state == 5) {
+            this.state = 7;
+        }
+        else if (this.state == 6) {
+            this.state = 8;
+        }
+    }
+
+    checkGameEndRequestOver() {
+        if(this.state == 5) {
+            this.state = 6;
+        }
+        else if (this.state == 7) {
+            this.state = 8;
+        }
+    }
+
+    updateTurnsRequestOver() {
+        if(this.state == 8) {
+            this.state = 9;
+        }
+    }
+
+    dropAnimationOver() {
+        if(this.state == 9) {
+            this.reset();
+        }
+    }
+
+    reset(){
         this.piece = null;
         this.destinationTile = null;
         this.state = 0;
